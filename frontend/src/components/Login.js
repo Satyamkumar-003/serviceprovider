92447a4489d2b2285f834a877c232760b66a84fd
Satyamkumar-003 <satyamkumar0238@gmail.com>
2025-11-10 18:51:49 +0530

    link not used in login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 import "./Login.css";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export default function Login() {
   const navigate=useNavigate();
